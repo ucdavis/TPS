0ddfa247a423306bef80882c13bb8f1f40fd3ace
Scott Kirkland <srkirkland@gmail.com>
2012-05-07 17:10:42 -0700

    changed account to use CAS, added base appcontroller

---
+++ TPS.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+using UCDArch.Web.Attributes;
+using UCDArch.Web.Controller;
+
+namespace TPS.Web.Controllers
+{
+    [Authorize]
+    [Version(MajorVersion = 4)] //TPS version, maybe 4?
+    public class ApplicationController : SuperController { }
+}
--- TPS.Web/Controllers/HomeController.cs
+++ TPS.Web/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UCDArch.Web.Attributes;
 
 namespace TPS.Web.Controllers
 {
-    public class HomeController : Controller
+    [HandleTransactionsManually] //No transactions if you don't need to access db in home controller
+    public class HomeController : ApplicationController
     {
         public ActionResult Index()
         {
