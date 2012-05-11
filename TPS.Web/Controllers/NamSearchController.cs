aaece7ef65c35cdb0aff2978060dd7fa9c80833d
taylor <kentaylor@ucdavis.edu>
2012-05-11 12:42:22 -0700

    Moved query logic from NamSearchModel to NamSearchController.

--- TPS.Web/Controllers/NamSearchController.cs
+++ TPS.Web/Controllers/NamSearchController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using TPS.Core.Domain;
+using TPS.Web.Helpers;
+using TPS.Web.Models;
 using UCDArch.Core.PersistanceSupport;
 
 namespace TPS.Web.Controllers
@@ -14,10 +15,20 @@ namespace TPS.Web.Controllers
 
         private readonly IRepositoryWithTypedId<DataNam, int> _dataNamRepository;
 
-        public NamSearchController(IRepositoryWithTypedId<Vlan, int> vlanRepository, IRepositoryWithTypedId<DataNam, int> dataNamRepository)
+        private readonly IRepositoryWithTypedId<Building, int> _buildingRepository;
+
+        private readonly IRepositoryWithTypedId<Department, string> _departmentRepository;
+
+        public System.Linq.Expressions.Expression<Func<DataNam, bool>> NamSearchExpression { get; set; }
+
+        public NamSearchController(IRepositoryWithTypedId<Vlan, int> vlanRepository, IRepositoryWithTypedId<DataNam, int> dataNamRepository,
+            IRepositoryWithTypedId<Building, int> buildingRepository, IRepositoryWithTypedId<Department, string> departmentRepository
+            )
         {
             _vlanRepository = vlanRepository;
             _dataNamRepository = dataNamRepository;
+            _buildingRepository = buildingRepository;
+            _departmentRepository = departmentRepository;
         }
 
         //
@@ -25,16 +36,69 @@ namespace TPS.Web.Controllers
 
         public ActionResult Index()
         {
-            var namSearchModel = Models.NamSearchModel.Create(Repository);
+            var viewModel = new NamSearchModel
+                                {
+                                    DataNams = new List<DataNam>(),
+                                    Vlans = _vlanRepository.Queryable.OrderBy(t => t.Name).ToList(),
+                                    Buildings = _buildingRepository.Queryable.OrderBy(t => t.Name).ToList(),
+                                    Departments =
+                                        _departmentRepository.Queryable.OrderBy(t => t.Name).ToList()
+                                };
 
-            return View(namSearchModel);
+            return View(viewModel);
         }
 
-        public ActionResult SearchResults(string namNumber, string roomNumber, string vlan, string building, string department)
+        public ActionResult SearchResults(string namNumber, string roomNumber, string vlanId, string buildingId, string departmentId)
         {
-            var namSearchModel = Models.NamSearchModel.Create(Repository, namNumber, roomNumber, vlan, building, department);
+            var hasNamNumber = !(String.IsNullOrEmpty(namNumber) || namNumber.Equals("0"));
+            var hasRoomNumber = !(String.IsNullOrEmpty(roomNumber) || roomNumber.Equals("0"));
+            var hasVlanId = !(String.IsNullOrEmpty(vlanId) || vlanId.Equals("0"));
+            var hasBuildingId = !(String.IsNullOrEmpty(buildingId) || buildingId.Equals("0"));
+            var hasDepartmentId = !(String.IsNullOrEmpty(departmentId) || departmentId.Equals("0"));
+            var hasSearchParameters = hasNamNumber || hasRoomNumber || hasVlanId || hasBuildingId || hasDepartmentId
+                                          ? true
+                                          : false;
 
-            return View(namSearchModel);
+            var viewModel = new NamSearchModel
+                                {
+                                    DataNams = new List<DataNam>()
+                                };
+
+            if (hasSearchParameters)
+            {
+                NamSearchExpression = PredicateBuilder.True<DataNam>();
+
+                if (hasNamNumber)
+                {
+                    NamSearchExpression = NamSearchExpression.And(p => p.NamNumber.Equals(namNumber));
+                }
+                if (hasRoomNumber)
+                {
+                    NamSearchExpression = NamSearchExpression.And(p => p.Room.Equals(roomNumber));
+                }
+                if (hasVlanId)
+                {
+                    NamSearchExpression =
+                        NamSearchExpression.And(p => p.Vlan.Id == int.Parse(vlanId));
+                }
+                if (hasBuildingId)
+                {
+                    NamSearchExpression =
+                        NamSearchExpression.And(p => p.Building.Id == int.Parse(buildingId));
+                }
+                if (hasDepartmentId)
+                {
+                    NamSearchExpression =
+                        NamSearchExpression.And(p => p.Department.Id.Equals(departmentId));
+                }
+
+                viewModel.DataNams = _dataNamRepository
+                    .Queryable
+                    .Where(NamSearchExpression)
+                    .OrderBy(t => t.NamNumber)
+                    .ToList();
+            }
+            return View(viewModel);
         }
 
         public ActionResult DisplayNam(int id)
--- TPS.Web/Models/NamSearchModel.cs
+++ TPS.Web/Models/NamSearchModel.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using TPS.Core.Domain;
-using TPS.Web.Helpers;
-using UCDArch.Core.PersistanceSupport;
-using UCDArch.Core.Utils;
 
 namespace TPS.Web.Models
 {
     public class NamSearchModel
     {
-        public System.Linq.Expressions.Expression<Func<DataNam, bool>> NamSearchExpression { get; set; }
-
         public string NamNumber { get; set; }
 
         public Vlan Vlan { get; set; }
@@ -30,68 +23,5 @@ namespace TPS.Web.Models
         { get; set; }
 
         public IList<DataNam> DataNams { get; set; }
-
-        public static NamSearchModel Create(IRepository repository)
-        {
-            return Create(repository, null, null, null, null, null);
-        }
-
-        public static NamSearchModel Create(IRepository repository, string namNumber, string roomNumber, string vlanId, string buildingId, string departmentId)
-        {
-            Check.Require(repository != null, "Repository must be supplied");
-
-            var hasNamNumber = !(String.IsNullOrEmpty(namNumber) || namNumber.Equals("0"));
-            var hasRoomNumber = !(String.IsNullOrEmpty(roomNumber) || roomNumber.Equals("0"));
-            var hasVlanId = !(String.IsNullOrEmpty(vlanId) || vlanId.Equals("0"));
-            var hasBuildingId = !(String.IsNullOrEmpty(buildingId) || buildingId.Equals("0"));
-            var hasDepartmentId = !(String.IsNullOrEmpty(departmentId) || departmentId.Equals("0"));
-            var hasSearchParameters = hasNamNumber || hasRoomNumber || hasVlanId || hasBuildingId || hasDepartmentId
-                                          ? true
-                                          : false;
-
-            var viewModel = new NamSearchModel
-                                {
-                                    DataNams = new List<DataNam>(),
-                                    Vlans = repository.OfType<Vlan>().Queryable.OrderBy(t => t.Name).ToList(),
-                                    Buildings = repository.OfType<Building>().Queryable.OrderBy(t => t.Name).ToList(),
-                                    Departments = repository.OfType<Department>().Queryable.OrderBy(t => t.Name).ToList()
-                                };
-
-            if (hasSearchParameters)
-            {
-                viewModel.NamSearchExpression = PredicateBuilder.True<DataNam>();
-
-                if (hasNamNumber)
-                {
-                    viewModel.NamSearchExpression = viewModel.NamSearchExpression.And(p => p.NamNumber.Equals(namNumber));
-                }
-                if (hasRoomNumber)
-                {
-                    viewModel.NamSearchExpression = viewModel.NamSearchExpression.And(p => p.Room.Equals(roomNumber));
-                }
-                if (hasVlanId)
-                {
-                    viewModel.NamSearchExpression = viewModel.NamSearchExpression.And(p => p.Vlan.Id == int.Parse(vlanId));
-                }
-                if (hasBuildingId)
-                {
-                    viewModel.NamSearchExpression =
-                        viewModel.NamSearchExpression.And(p => p.Building.Id == int.Parse(buildingId));
-                }
-                if (hasDepartmentId)
-                {
-                    viewModel.NamSearchExpression =
-                       viewModel.NamSearchExpression.And(p => p.Department.Id.Equals(departmentId));
-                }
-
-                viewModel.DataNams = repository.OfType<DataNam>()
-                    .Queryable
-                    .Where(viewModel.NamSearchExpression)
-                    .OrderBy(t => t.NamNumber)
-                    .ToList();
-            }
-
-            return viewModel;
-        }
     }
 }
